1f4a7a9464ae5bb68986ab1cc5a3461586288e63
John Chrosniak <chrosniakj@gmail.com>
2024-06-13 16:03:30 -0400

    copy parameters to bag directory on shutdown

--- launch/ros2_rosbag.launch.py
+++ launch/ros2_rosbag.launch.py
@@ -17,7 +17,8 @@ from launch_ros.actions import Node
 from launch.actions import OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler
+from launch.event_handlers import OnShutdown
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
@@ -25,18 +26,22 @@ from datetime import datetime
 import pathlib
 import yaml
 import os
+import shutil
 
 # This function is used to generate a command to record a ROS 2 rosbag that excludes topics
 # topics as provided in the appropriate configuration file.
-def record_ros2_rosbag(context: LaunchContext, param_file):
 
+bag_dir = ''
+
+def record_ros2_rosbag(context: LaunchContext, param_file):
+    global bag_dir
     # Open vehicle config params file to process various rosbag settings
     with open(param_file, 'r') as f:
 
         params = yaml.safe_load(f)
-
+        bag_dir = '/home/ubuntu/bags/rosbag2_' + str(datetime.now().strftime('%Y-%m-%d_%H%M%S'))
         proc = ExecuteProcess(
-            cmd=['ros2', 'bag', 'record', '-o', '/home/ubuntu/bags/rosbag2_' + str(datetime.now().strftime('%Y-%m-%d_%H%M%S')), *params['ros2_rosbag']['ros__parameters']['topics']],
+            cmd=['ros2', 'bag', 'record', '-o', bag_dir, *params['ros2_rosbag']['ros__parameters']['topics']],
             output='screen',
             shell='true'
             )
@@ -44,7 +49,14 @@ def record_ros2_rosbag(context: LaunchContext, param_file):
         return [proc]
 
 
+def on_shutdown(event, context):
+    params_file = os.path.join(get_package_share_directory("c1t_bringup"), "params", "params.yaml")
+    shutil.copy(params_file, bag_dir)
+
 def generate_launch_description():
     return LaunchDescription([
-        OpaqueFunction(function=record_ros2_rosbag, args=[os.path.join(get_package_share_directory("c1t_bringup"), "params", "params.yaml")])
+        OpaqueFunction(function=record_ros2_rosbag, args=[os.path.join(get_package_share_directory("c1t_bringup"), "params", "params.yaml")]),
+        RegisterEventHandler(
+            OnShutdown(on_shutdown=on_shutdown)
+        )
     ])
