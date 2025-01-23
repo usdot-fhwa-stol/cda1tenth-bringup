ad13d678fe902cfd16740266414aca0439b00e16
John Chrosniak <chrosniakj@gmail.com>
2025-01-23 12:48:53 -0500

    added startup scripts to engage the port drayage demonstration

---
+++ startup_scripts/start_garage_demo.sh
@@ -0,0 +1,15 @@
+ros2 topic pub --once /goal_pose geometry_msgs/msg/PoseStamped "header:
+  stamp:
+    sec: 0
+    nanosec: 0
+  frame_id: ''
+pose:
+  position:
+    x: 0.55
+    y: -2.45
+    z: 0.0
+  orientation:
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0"
---
+++ startup_scripts/start_trb_demo.sh
@@ -0,0 +1,15 @@
+ros2 topic pub --once /goal_pose geometry_msgs/msg/PoseStamped "header:
+  stamp:
+    sec: 0
+    nanosec: 0
+  frame_id: ''
+pose:
+  position:
+    x: -1.6
+    y: -0.2
+    z: 0.0
+  orientation:
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0"
