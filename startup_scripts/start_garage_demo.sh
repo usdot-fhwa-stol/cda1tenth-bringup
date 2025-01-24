4835f16436d97d62befcd1687f3b15af6c10b529
John Chrosniak <chrosniakj@gmail.com>
2025-01-24 15:14:12 -0500

    Cf 966 route operations (#48)
    * Added trigger for port drayage ack to turtlebot map
    
    * Changed user to humble, renamed and updated graph for multi path testing
    
    * added operations to port drayage graph
    
    * added entrance_reached action to params
    
    * removed unused operation
    
    * added entrance operation to trb graph
    
    * added startup scripts to engage the port drayage demonstration
    
    * switched startup scripts to publish moms
    
    ---------
    
    Co-authored-by: Annika Frye <annika@frenchfryes.com>
    Co-authored-by: Peyton Johnson <willjohnsonk@gmail.com>

---
+++ startup_scripts/start_garage_demo.sh
@@ -0,0 +1,8 @@
+ros2 topic pub --once /incoming_mobility_operation carma_v2x_msgs/msg/MobilityOperation "m_header:
+  sender_id: ''
+  recipient_id: ''
+  sender_bsm_id: ''
+  plan_id: ''
+  timestamp: 0
+strategy: 'carma/port_drayage'
+strategy_params: '{\"cmv_id\":\"RED-TRUCK\",\"operation\":\"ENTER_PORT\",\"cargo\":false,\"cargo_id\":\"SOME_CARGO\",\"destination\":{\"longitude\":\"0.55\",\"latitude\":\"-2.45\"},\"action_id\":\"SOMEUID\"}'"
