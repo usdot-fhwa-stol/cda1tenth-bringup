8b7c11b1d3449f05a986d2b9213c8a0a8c05c57b
John Chrosniak <chrosniakj@gmail.com>
2025-01-24 14:22:59 -0500

    switched startup scripts to publish moms

--- startup_scripts/start_garage_demo.sh
+++ startup_scripts/start_garage_demo.sh
@@ -1,15 +1,8 @@
-ros2 topic pub --once /goal_pose geometry_msgs/msg/PoseStamped "header:
-  stamp:
-    sec: 0
-    nanosec: 0
-  frame_id: ''
-pose:
-  position:
-    x: 0.55
-    y: -2.45
-    z: 0.0
-  orientation:
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0"
+ros2 topic pub --once /incoming_mobility_operation carma_v2x_msgs/msg/MobilityOperation "m_header:
+  sender_id: ''
+  recipient_id: ''
+  sender_bsm_id: ''
+  plan_id: ''
+  timestamp: 0
+strategy: 'carma/port_drayage'
+strategy_params: '{\"cmv_id\":\"RED-TRUCK\",\"operation\":\"ENTER_PORT\",\"cargo\":false,\"cargo_id\":\"SOME_CARGO\",\"destination\":{\"longitude\":\"0.55\",\"latitude\":\"-2.45\"},\"action_id\":\"SOMEUID\"}'"
