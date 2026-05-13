a5207e52c7e17a4d99061611d542728a04f4fe15
rahulandurkar <80483401+rahulandurkar@users.noreply.github.com>
2026-05-13 10:05:26 -0400

    Update entrypoint.sh to source additional setups

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
-source /opt/ros/humble/setup.bash          
-source /opt/underlay_ws/install/setup.bash 
-source /opt/overlay_ws/install/setup.bash  
-source /opt/nav2_ext_ws/install/setup.bash     
-source /opt/bringup_ws/install/setup.bash 
+source /opt/ros/humble/setup.bash
+source /opt/underlay_ws/install/setup.bash
+source /opt/overlay_ws/install/setup.bash
+source /home/carma/.base-image/workspace/install/setup.bash
+source /home/carma/.base-image/ros2_msgs_ws/install/setup.bash
+source /opt/carma/install/setup.bash
+source /opt/nav2_ext_ws/install/setup.bash
+source /opt/bringup_ws/install/setup.bash
 
 exec "$@"
